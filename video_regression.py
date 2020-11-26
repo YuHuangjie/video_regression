ba26cd22d2baf7f4f9470494ea692fe343655863
YuHuangjie <yuhj@shanghaitech.edu.cn>
2020-11-26 04:45:49 +0000

    tune feature size

--- video_regression.py
+++ video_regression.py
@@ -185,8 +185,8 @@ elif args.model_type == 'ffm':
 elif args.model_type == 'rff':
     if model_params is None:
         # W = rbf_sample(5e2, 2e2, 1e3, 8192)
-        W = exp_sample(30, 30, 30*video.shape[1]/video.shape[0], 8192*2)
-        b = np.random.uniform(0, np.pi * 2, 8192*2)
+        W = exp_sample(30, 30, 30*video.shape[1]/video.shape[0], 8192)
+        b = np.random.uniform(0, np.pi * 2, 8192)
     else:
         W, b = model_params
     model = make_rff_network(*network_size, W, b)
